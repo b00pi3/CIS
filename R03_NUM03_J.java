2fdff74726c193b45796a83f511eee97675aa2ee
b00pi3 <149516630+b00pi3@users.noreply.github.com>
2024-03-02 22:12:47 -0500

    Update R03_NUM03_J.java
    Makes values readable 32-bit unsigned integers.

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -5,6 +5,6 @@ import java.io.IOException;
 public class R03_NUM03_J {
 
     public static int getInteger(DataInputStream is) throws IOException {
-        return is.readInt();
+        return is.readInt() & 0xFFFFFFFFL;
     }
 }
