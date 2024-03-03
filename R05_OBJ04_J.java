63b19058daeb6e9f6592e197efb75d1c58688811
b00pi3 <149516630+b00pi3@users.noreply.github.com>
2024-03-02 22:50:37 -0500

    Add files via upload

---
+++ R05_OBJ04_J.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+public class R05_OBJ04_J {
+    private Date date;
+
+    public R05_OBJ04_J(Date d) {
+        this.date = d;
+    }
+
+    public void setDate(Date d) {
+        this.date = d;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+}
