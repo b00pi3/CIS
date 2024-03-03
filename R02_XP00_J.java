e355ddf3cbb0ec7d86cf2e302b46592a7048357a
b00pi3 <149516630+b00pi3@users.noreply.github.com>
2024-03-02 22:03:55 -0500

    Update R02_XP00_J.java
    Compliant solution correctly updates String reference original.

--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -2,7 +2,7 @@ public class R02_XP00_J {
 
     public static void main(String[] args) {
         String original = "insecure";
-        original.replace('i', '9');
+        original = original.replace('i', '9');
         System.out.println(original);
     }
 }
