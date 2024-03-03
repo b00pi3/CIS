4aba46a52d9cb89de92e61a26cdcbabe39ad8e60
b00pi3 <149516630+b00pi3@users.noreply.github.com>
2024-03-02 23:37:49 -0500

    Update R06_MET04_J.java
    Sllution declares to doLogic() method final to prevent malicious overriding.

--- R06_MET04_J.java
+++ R06_MET04_J.java
@@ -1,14 +1,8 @@
 public class R06_MET04_J {
     class Super {
-        protected void doLogic() {
-            System.out.println("Super invoked");
-        }
-    }
-
-    public class Sub extends Super {
-        public void doLogic() {
-            System.out.println("Sub invoked");
-            // Do sensitive operations
-        }
-    }
+  protected final void doLogic() { // Declare as final
+    System.out.println("Super invoked");
+    // Do sensitive operations
+  }
+}
 }
