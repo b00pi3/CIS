c6523ac64247e8c8823787974f0d4a2641dadb55
b00pi3 <149516630+b00pi3@users.noreply.github.com>
2024-03-02 23:16:43 -0500

    Add files via upload

---
+++ R14_SER00_J.java
@@ -0,0 +1,13 @@
+import java.io.Serializable;
+
+public static R14_SER00_J {
+
+class GameWeapon implements Serializable{
+
+        int numOfWeapons = 10;
+
+        public String tostring() {
+            return String.valueOf(numOfWeapons);
+        }
+    }
+}
