fd1204d58a45fd7476e98bb262a19866f0a33d9f
b00pi3 <149516630+b00pi3@users.noreply.github.com>
2024-03-02 23:28:13 -0500

    Add files via upload

---
+++ R02_EXP05_J.java
@@ -0,0 +1,20 @@
+public class R02_EXP05_J{
+        public static void main(String[] args) {
+          int number = 17;
+          int threshold = 10;
+          number = (number > threshold ? 0 : -2)
+                   + ((31 * ++number) * (number = get()));
+          // ...
+          if (number == 0) {
+            System.out.println("Access granted");
+          } else {
+            System.out.println("Denied access"); // number = -2
+          }
+        }
+       
+        public static int get() {
+          int number = 0;
+          // Assign number to nonzero value if authorized, else 0
+          return number;
+        }
+}
