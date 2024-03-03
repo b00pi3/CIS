8544be5d004b4703300bf07e1da9081c3da30817
b00pi3 <149516630+b00pi3@users.noreply.github.com>
2024-03-02 22:22:17 -0500

    Update R04_STR03_J.java
    covert string to byte array

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -4,7 +4,7 @@ import java.math.BigInteger;
     public static void main( String [] args){
 
         BigInteger x = new BigInteger("520500452766");
-        byte[] byteArray = x.toByteArray();
+        String s = x.toString(); // valid char data
         String s = new String(byteArray);
         byteArray = s.getBytes();
         x = new BigInteger(byteArray);
