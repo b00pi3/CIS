3ad37a3bc1ce64c53475c0af433ce6d1902fe092
b00pi3 <149516630+b00pi3@users.noreply.github.com>
2024-03-02 23:05:18 -0500

    Update R03_NUM14_J.java
    Fix logical shift operator.

--- R03_NUM14_J.java
+++ R03_NUM14_J.java
@@ -4,7 +4,7 @@ public class R03_NUM14_J{
             int bits = 0;
             while( value != 0) {
                 bits += value &1l;
-                value >>= 1;//signed right shift, by 1
+                value >>>= 1;
             }
             return bits;
         } 
