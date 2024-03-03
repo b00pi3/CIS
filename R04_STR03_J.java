9743b9f9cfaaf754724f7e2aaaac0bca5f2c889e
b00pi3 <149516630+b00pi3@users.noreply.github.com>
2024-03-02 22:20:25 -0500

    Add files via upload

---
+++ R04_STR03_J.java
@@ -0,0 +1,13 @@
+import java.math.BigInteger;
+ class R04_STR03_J {
+
+    public static void main( String [] args){
+
+        BigInteger x = new BigInteger("520500452766");
+        byte[] byteArray = x.toByteArray();
+        String s = new String(byteArray);
+        byteArray = s.getBytes();
+        x = new BigInteger(byteArray);
+
+    }
+}
