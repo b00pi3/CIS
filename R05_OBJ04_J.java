f40bd39beae9b93549bbf9cdc0c9b0da4b822b31
b00pi3 <149516630+b00pi3@users.noreply.github.com>
2024-03-02 22:56:15 -0500

    Update R05_OBJ04_J.java
    Provide mutable classes with copy functionality to safely allow passing instances to untrusted code

--- R05_OBJ04_J.java
+++ R05_OBJ04_J.java
@@ -1,17 +1,17 @@
 import java.util.Date;
 
 public class R05_OBJ04_J {
-    private Date date;
+    private final Date date;
 
     public R05_OBJ04_J(Date d) {
-        this.date = d;
+        this.date = new Date(mc.date.getTIme());
     }
 
     public void setDate(Date d) {
-        this.date = d;
+        this.date = new Date(d.getTIme()); //make defensive copy
     }
 
     public Date getDate() {
-        return date;
+        return (Date) date.clone();//copy and return 
     }
 }
