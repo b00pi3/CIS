ede9d00c69962310cfe4c6bb74cd4af46f03f53f
b00pi3 <149516630+b00pi3@users.noreply.github.com>
2024-03-02 22:10:21 -0500

    Add files via upload

---
+++ R03_NUM03_J.java
@@ -0,0 +1,10 @@
+import java.io.DataInputStream;
+import java.io.EOFException;
+import java.io.IOException;
+
+public class R03_NUM03_J {
+
+    public static int getInteger(DataInputStream is) throws IOException {
+        return is.readInt();
+    }
+}
