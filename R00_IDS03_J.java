cb0b3924b9c40446b50eced4ddb3ec347fe2502e
b00pi3 <149516630+b00pi3@users.noreply.github.com>
2024-03-02 21:39:56 -0500

    Update R00_IDS03_J.java
    Code fixed to avoid logging unsanitized user input.

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,4 +1,4 @@
-// RULE 00. INPUT VALIDATION AND DATA SANITZATION
+// RULE 00. INPUT VALIDATION AND DATA SANITIZATION
 
 import java.util.logging.Logger;
 
@@ -12,9 +12,9 @@ public class R00_IDS03_J {
         String username = "gaB33_21";
 
         if (loginSuccessful) {
-            logger.severe("User login succeeded for: " + username);
+            logger.severe("User login succeeded for: " + sanatizeUser(username));
         } else {
-            logger.severe("User login failed for: " + username);
+            logger.severe("User login failed for: " + sanatizeUser(username);
         }
     }
 
@@ -24,4 +24,8 @@ public class R00_IDS03_J {
         // Replace this with your actual authentication logic
         return username.equals("gaB33_21") && password.equals("ThatkoolKidd#23");
     }
+
+    public String sanitizeUser(String name) {
+        return Pattern.matches("[A-Za-z0-9]+", username))
+            ? username : "unauthorized user";
 }
