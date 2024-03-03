8ee462dc8a0351fbae2eee38f56a7d487e079d28
b00pi3 <149516630+b00pi3@users.noreply.github.com>
2024-03-02 22:37:25 -0500

    Update R06_MET01_J.java
    Validate method arguments.

--- R06_MET01_J.java
+++ R06_MET01_J.java
@@ -1,10 +1,14 @@
 public class R06_MET01_J {
     public static int getAbsAdd(int x, int y) {
-        assert x != Integer.MIN_VALUE;
-        assert y != Integer.MIN_VALUE;
-        int absx = Math.abs(x);
-        int absy = Math.abs(y);
-        assert (absx <= Integer.MAX_VALUE - absy);
+        if(x == Integer.MIN_VALUE || y == Integer.MIN_VALUE){
+            throw new IllegalArgumentException();
+        }    
+        int absX = Math.abs(x);
+        int absY = Math.abs(y);
+        if (absX > Integer.MAX_VALUE - absY){
+            throw new IllegalArgumentException();
+        }
+        
         return absx + absy;
     }
 }
